c0eda56026ba992a5c0bf3d755bea80896f84af7
王东升 <790116273@qq.com>
2015-10-06 17:11:22 +0800

    init

---
+++ QRScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRScanner
+//
+//  Created by tls on 15/10/6.
+//  Copyright (c) 2015年 tls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
